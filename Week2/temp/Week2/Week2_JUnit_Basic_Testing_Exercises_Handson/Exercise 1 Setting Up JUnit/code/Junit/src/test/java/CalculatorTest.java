8613fd681ccbf779a0de85cbc5d1461dd8b91196
prasanna2309 <prasanna.vegr@gmail.com>
2025-06-29 11:05:50 +0530

    Add files via upload

---
+++ Week2/temp/Week2/Week2_JUnit_Basic_Testing_Exercises_Handson/Exercise 1 Setting Up JUnit/code/Junit/src/test/java/CalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        int sum = 2 + 3;
+        assertEquals(5, sum);
+    }
+}
---
+++ Week2/temp/Week2/Week2_MockitoExercises_Handson/Ex2_Verifying Interactions/code/mockexample2/mockexample/src/main/java/myapp/ExternalApi.java
@@ -0,0 +1,5 @@
+package myapp;
+
+public interface ExternalApi {
+    String getData();
+}
